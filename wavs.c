51f0230db02fdf2c7f3ce5d3743094d99ad9d928
Mr Krot <vkorotanec@gmail.com>
2026-06-30 01:02:03 +0200

    Bidirectional audio

--- wavs.c
+++ wavs.c
@@ -17,16 +17,26 @@ typedef struct {
   uint16_t bits_per_sample;
 } Fmt;
 
-Fmt fmt_make(uint32_t file_frequency) {
+typedef struct {
+  uint16_t channels;
+  uint32_t file_frequency;
+  uint16_t bits_per_sample;
+} SampleDataInfo;
+
+Fmt fmt_make(SampleDataInfo* info, uint32_t file_frequency) {
   Fmt r = {0};
   r.tag = TAG("fmt ");
   r.size = 0x10;
   r.audio_format = 1; // 1 - int, 3 - float
-  r.channels = 1;
+  r.channels = 2;
   r.file_frequency = file_frequency;
   r.bits_per_sample = 8;
   r.byte_per_bloc = r.channels * r.bits_per_sample / 8;
   r.byte_per_sec = r.byte_per_bloc * r.file_frequency;
+
+  info->channels =        r.channels;
+  info->file_frequency =  file_frequency;
+  info->bits_per_sample = r.bits_per_sample;
 
   return r;
 }
@@ -41,27 +51,37 @@ typedef struct {
   uint8_t* data;
 } SampleData;
 
-SampleData sample_make(uint32_t file_frequency) {
+SampleData sample_make(SampleDataInfo* info) {
   SampleData r = {0};
   r.bloc_id = TAG("data");
 
   const uint32_t file_time = 4; // seconds
+  const size_t data_size =
+    info->file_frequency * file_time * info->channels;
+  
+  r.size = data_size;
+  r.data = malloc(data_size);
 
-  r.size = file_frequency * file_time;
-  r.data = malloc(file_frequency * file_time);
+  const float frequency = 200.0;
 
-  const float frequency = 1000.0;
+  for (size_t i = 0; i < data_size; i++) {
+    const float time = (float)i / (float)info->file_frequency;
 
-  for (size_t i = 0; i < file_frequency * file_time; i++) {
-    const float time = (float)i / (float)file_frequency;
-    const float x = sin(2 * M_PI * time * frequency);
+    float x;
+    if (i % 2 == 0) 
+      x =
+	(sin(2 * M_PI * time * frequency) / 2.0) +
+	(cos(2 * M_PI * time * frequency * 5) / 2.0);
+    else
+      x = sin(2 * M_PI * time * frequency * 10);
+    
     r.data[i] = (uint8_t)(((x + 1.0) / 2.0) * 255);
   }
 
   return r;
 }
 
-void sample_write(SampleData* sample, FILE* fd) {
+void sample_write(const SampleData* sample, FILE* fd) {
   fwrite(sample, sizeof(SampleData) - sizeof(uint8_t*), 1, fd);
   fwrite(sample->data, sample->size, 1, fd);
 }
@@ -80,8 +100,9 @@ Riff riff_make(uint32_t file_frequency) {
   r.tag = TAG("RIFF");
   r.file_format = TAG("WAVE");
 
-  r.fmt  = fmt_make(file_frequency);
-  r.data = sample_make(file_frequency);
+  SampleDataInfo info = {0};
+  r.fmt  = fmt_make(&info, file_frequency);
+  r.data = sample_make(&info);
 
   r.size = r.fmt.size + r.data.size;
 
