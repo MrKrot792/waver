dc16b794694ddfd4114b43a2396f87e273771108
Mr Krot <vkorotanec@gmail.com>
2026-08-11 16:26:37 +0200

    Fixed the NULL bug in wave_sqaure

--- src/wave.c
+++ src/wave.c
@@ -1,12 +1,14 @@
 #include <math.h>
+#include <stdlib.h>
 
 #include "wave.h"
 
-float wave_square(float x, float w, void* duty) {
+float wave_square(float x, float w, void* d) {
   w /= 2*M_PI;
   float phase = x * w;
   phase -= (int)phase;
-  return phase < *(float*)duty ? 1.0f : -1.0f;
+  float duty = d == NULL ? 0.5 : *(float*)d;
+  return phase < duty ? 1.0f : -1.0f;
 }
 
 float wave_sine(float x, float w, void* d) {
