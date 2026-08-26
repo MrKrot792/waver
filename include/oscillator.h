82e621191c0b3f30997f78e5a5a111e6a21bf77d
Mr Krot <vkorotanec@gmail.com>
2026-08-26 03:41:37 +0200

    Restructuring oscillator.c + oscillator_reset()

--- include/oscillator.h
+++ include/oscillator.h
@@ -4,11 +4,12 @@
 #include "inputs/inputs.h"
 
 typedef struct {
-  input_t* frequency;
-  input_t* amplitude;
-  wave_t   wave;
+  input_t frequency;
+  input_t amplitude;
+  wave_t  wave;
 } oscillator_t;
 
 float oscillator_get(const oscillator_t* osc);
 void  oscillator_tick(oscillator_t* osc);
+void  oscillator_reset(oscillator_t* osc);
 void  oscillator_deinit(oscillator_t* osc);
--- src/oscillator.c
+++ src/oscillator.c
@@ -3,20 +3,24 @@
 #include "wave.h"
 
 float oscillator_get(const oscillator_t* osc) {
-  // float f = input_get(osc->frequency);
-  float a = input_get(osc->amplitude);
+  float a = input_get(&osc->amplitude);
   return a * wave_get(&osc->wave);
 }
 
 void oscillator_tick(oscillator_t* osc) {
-  wave_tick(&osc->wave, input_get(osc->frequency));
-  input_tick(osc->frequency);
-  input_tick(osc->amplitude);
+  wave_tick(&osc->wave, input_get(&osc->frequency));
+  input_tick(&osc->frequency);
+  input_tick(&osc->amplitude);
+}
+
+void oscillator_reset(oscillator_t* osc) {
+  wave_reset(&osc->wave);
+  input_reset(&osc->frequency, NULL);
+  input_reset(&osc->amplitude, NULL);
 }
 
 void oscillator_deinit(oscillator_t* osc) {
-  input_deinit(osc->frequency);
-  input_deinit(osc->amplitude);
-  if (osc->wave.user_data.deinit_fn != NULL)
-    osc->wave.user_data.deinit_fn(osc->wave.user_data.data);
+  wave_deinit(&osc->wave);
+  input_deinit(&osc->frequency);
+  input_deinit(&osc->amplitude);
 }
